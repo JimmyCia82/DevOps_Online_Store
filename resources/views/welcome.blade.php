352adb627f971d86799fb039282aca73087c6e9e
Jimmy <jimmycia82@yahoo.com>
2018-08-06 12:00:57 +0700

    Test Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Jim
+                    Laravel Jim Test Git Ke dua
                 </div>
 
                 <div class="links">
